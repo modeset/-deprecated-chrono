9b2b174225f9c7ee89792f9e0ce41c07559c579b
Jay Zeschin <jay@zeschin.org>
2012-06-10 14:42:19 -0600

    Bring in responders gem

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.2.3'
 gem 'mysql2'
 gem 'omniauth-github'
 
+gem 'responders'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
+require "application_responder"
+
 class ApplicationController < ActionController::Base
+  self.responder = ApplicationResponder
+  respond_to :html
+
+
+
   protect_from_forgery
 
   protected
