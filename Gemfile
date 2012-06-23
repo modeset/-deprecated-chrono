2ca95e8b1bdb20d5a47fc68fe6b8fc88ea527a4e
Jay Zeschin <jay@zeschin.org>
2012-06-22 21:58:04 -0600

    Add underoos

--- Gemfile
+++ Gemfile
@@ -7,8 +7,10 @@ gem 'rails', '3.2.3'
 
 gem 'mysql2'
 gem 'omniauth-github'
+gem 'underoos'
 
 gem 'responders'
+gem 'haml-rails'
 
 # Gems used only for assets and not required
 # in production environments by default.
---
+++ app/assets/javascripts/underoos.js
@@ -0,0 +1,18 @@
+
+/* VENDOR ASSETS */
+//= require jquery
+//= require bootstrap-transition
+//= require bootstrap-alert
+//= require bootstrap-modal
+//= require bootstrap-dropdown
+//= require bootstrap-scrollspy
+//= require bootstrap-tab
+//= require bootstrap-tooltip
+//= require bootstrap-popover
+//= require bootstrap-button
+//= require bootstrap-collapse
+//= require bootstrap-typeahead
+
+
+/* APPLICATION ASSETS */
+
