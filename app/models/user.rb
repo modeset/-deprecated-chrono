a3d1326be43a9d654b7e0b26b8c060f2ce5cdbd6
Jay Zeschin <jay@zeschin.org>
2012-06-22 22:30:51 -0600

    Be more specific in typechecking for Postgres

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ActiveRecord::Base
   has_many :statuses, :dependent => :destroy
 
   def self.find_or_create_from_auth_hash(auth_hash)
-    User.find_or_create_by_uid(auth_hash[:uid], :name => auth_hash[:info][:name])
+    User.find_or_create_by_uid(auth_hash[:uid].to_s, :name => auth_hash[:info][:name])
   end
 end
