c1e56d0d817b3bb753b431ac7dc645d9514a926c
Jay Zeschin <jay@zeschin.org>
2012-06-24 23:25:30 -0600

    Split out token updating semantics

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :organizations
 
   def self.find_or_create_from_auth_hash(auth_hash)
-    user = User.find_or_create_by_uid(auth_hash[:uid].to_s, :name => auth_hash[:info][:name], :github_token => auth_hash[:credentials][:token])
+    user = User.find_or_create_by_uid(auth_hash[:uid].to_s, :name => auth_hash[:info][:name])
+    user.update_attributes :github_token => auth_hash[:credentials][:token]
     gh = Ghee.access_token user.github_token
     gh.orgs.each do |o|
       Rails.logger.info o.inspect
