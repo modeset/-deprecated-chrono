984f5814ec972dda2486985ae867df28213a8314
Jay Zeschin <jay@zeschin.org>
2012-06-24 23:22:37 -0600

    Logging

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ActiveRecord::Base
     user = User.find_or_create_by_uid(auth_hash[:uid].to_s, :name => auth_hash[:info][:name], :github_token => auth_hash[:credentials][:token])
     gh = Ghee.access_token user.github_token
     gh.orgs.each do |o|
+      Rails.logger.info o.inspect
       user.organizations << Organization.find_or_create_by_name(o['login'])
     end
     user
